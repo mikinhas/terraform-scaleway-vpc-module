d9e761e4c463cba180fbeeb142350a9c345f5704
Michael MACHADO <Michael MACHADO>
2023-03-31 17:51:21 +0200

    Fix typo

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     scaleway = {
       source  = "scaleway/scaleway"
-      version = "v2.14.1"
+      version = "2.14.1"
     }
   }
   required_version = ">= 0.13"
