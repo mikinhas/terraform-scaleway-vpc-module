402205d0efbdf9c6a289411c30852b64ede67a80
Michael MACHADO <Michael MACHADO>
2023-08-23 15:11:07 +0200

    feat(): Add vpc resource

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+resource "scaleway_vpc" "vpc" {
+  project_id = var.project_id
+  region     = var.region
+
+  name = var.vpc_name
+
+  tags = var.vpc_tags
+}
+
 resource "scaleway_vpc_private_network" "private_network" {
   name = var.name
   zone = var.zone
