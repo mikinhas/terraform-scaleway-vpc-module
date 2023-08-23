1fc89fd3bdc61270953dba34d6cf59ca498ec820
Michael MACHADO <Michael MACHADO>
2023-08-23 15:11:07 +0200

    feat(): Add vpc resource

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
+output "vpc_id" {
+  value = scaleway_vpc.vpc.id
+}
+
 output "private_network_id" {
   value = scaleway_vpc_private_network.private_network.id
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,14 @@
+variable "project_id" {
+  type        = string
+  description = "The project id"
+  default     = null
+}
+
+variable "region" {
+  type    = string
+  default = null
+}
+
 variable "zone" {
   type        = string
   description = "Name of region zone"
@@ -8,4 +19,19 @@ variable "name" {
   type        = string
   description = "Name of the network"
   default     = ""
+}
+
+
+
+variable "vpc_name" {
+  type        = string
+  description = "Vpc name"
+  default     = "vpc01"
+}
+
+variable "vpc_tags" {
+  type        = list(string)
+  description = "List of vpc tags"
+  default     = []
+
 }
